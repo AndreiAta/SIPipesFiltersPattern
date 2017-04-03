42849c93ba0c5761bcf620681135509544ba9f1b
AndreiAta <andrei.atanasiu1994@gmail.com>
2017-04-03 09:26:02 +0200

    Added a CapitalizeFilter

--- src/CapitalizeFilter.java
+++ src/CapitalizeFilter.java
@@ -1,6 +1,22 @@
-/**
- * Created by Andre_000 on 02-04-2017.
- */
-public class CapitalizeFilter
+
+public class CapitalizeFilter implements Filter<StringPipe, StringPipe>
 {
+    @Override
+    public StringPipe process(StringPipe inputStream)
+    {
+        StringPipe outputStream = new StringPipe();
+        boolean capitalized = false;
+
+        while(inputStream.hasNext())
+        {
+            String message = (String) inputStream.pop();
+            if(capitalized = false)
+            {
+                message = message.substring(0, 1).toUpperCase() + message.substring(1);
+                capitalized = true;
+            }
+            outputStream.push(message);
+        }
+        return outputStream;
+    }
 }
